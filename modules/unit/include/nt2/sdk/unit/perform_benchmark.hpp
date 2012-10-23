f46ccb7a99c33d14b47210ba16a9879dd0fd746f
Domagoj Saric <domagoj.saric@littleendian.com>
2012-10-23 12:19:53 +0200

    Further improved stability/accuracy of the timing functions overhead (still not "perfect", especially for now()). Optimized the median<>() function template and (temporarily) moved it into the now.cpp. Switched to the rand48 PRNG (as it is faster) and (temporarily) moved the reusable PRNG functions into the now.cpp. Minor other refactoring and stylistic changes.

--- modules/unit/include/nt2/sdk/unit/perform_benchmark.hpp
+++ modules/unit/include/nt2/sdk/unit/perform_benchmark.hpp
@@ -9,51 +9,28 @@
 #ifndef NT2_SDK_UNIT_PERFORM_BENCHMARK_HPP_INCLUDED
 #define NT2_SDK_UNIT_PERFORM_BENCHMARK_HPP_INCLUDED
 
-#include <vector>
-#include <algorithm>
 #include <nt2/sdk/timing/now.hpp>
+
+#include <vector>
+#include <utility>
 
 namespace nt2 { namespace unit
 {
   typedef std::pair<nt2::details::cycles_t, nt2::details::microseconds_t> benchmark_result_t;
 
-  template <typename T> inline
-  T median( std::vector<T> & data )
-  {
-    typedef typename std::vector<T>::iterator iterator;
-    iterator const begin( data.begin() );
-    iterator const end  ( data.end  () );
-
-    std::size_t const size = end - begin;
-    std::size_t const middleIdx = size/2;
-    iterator const target = begin + middleIdx;
-    std::nth_element(begin, target, end);
-
-    if ( size % 2 )
-        return *target;
-    else
-    {
-        T const a = *target;
-        iterator const targetNeighbor = target-1;
-        std::nth_element(begin, targetNeighbor, end);
-        return (a+*targetNeighbor)/T(2);
-    }
-  }
-
-  static std::vector<nt2::details::cycles_t      > individual_measurement_cycles ;
-  static std::vector<nt2::details::microseconds_t> individual_measurement_seconds;
+  static std::vector<nt2::details::cycles_t      > individual_measurement_cycles       ;
+  static std::vector<nt2::details::time_quantum_t> individual_measurement_time_quantums;
 
   template <class Test> inline
   benchmark_result_t perform_benchmark( Test & test, nt2::details::seconds_t const total_duration_in_seconds )
   {
-    individual_measurement_cycles .clear();
-    individual_measurement_seconds.clear();
+    individual_measurement_cycles       .clear();
+    individual_measurement_time_quantums.clear();
 
     using namespace nt2::details;
 
-    microseconds_t const total_duration( total_duration_in_seconds * 1000000 );
-    microseconds_t       duration      ( 0                                   );
-
+    time_quantum_t const total_duration( to_timequantums( total_duration_in_seconds * 1000000 ) );
+    time_quantum_t       duration      ( 0                                                      );
 
     while ( duration < total_duration )
     {
@@ -76,18 +53,17 @@ namespace nt2 { namespace unit
         time_quantum_t const elapsed_time ( time_end   - time_start   - now_overhead                                       );
         cycles_t       const burned_cycles( cycles_end - cycles_start - read_cycles_overhead - ( 2 * now_overhead_cycles ) );
     */
-        seconds_t const elapsed_time_in_microseconds( to_microseconds( elapsed_time ) );
-        duration += elapsed_time_in_microseconds;
+        duration += elapsed_time;
 
-        individual_measurement_cycles .push_back( burned_cycles                );
-        individual_measurement_seconds.push_back( elapsed_time_in_microseconds );
+        individual_measurement_cycles       .push_back( burned_cycles );
+        individual_measurement_time_quantums.push_back( elapsed_time  );
         test.reset();
     }
 
     return benchmark_result_t
     (
-        median( individual_measurement_cycles  ),
-        median( individual_measurement_seconds )
+                         median( individual_measurement_cycles        ),
+        to_microseconds( median( individual_measurement_time_quantums ) )
     );
   }
 
