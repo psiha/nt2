4f23e9d82a74c5e74ae0dc395ee6d02054918545
Domagoj Saric <domagoj.saric@littleendian.com>
2012-10-23 13:48:50 +0200

    Created a virtual interface class for benchmarks. Used it to make the perform_benchmark function a non template and (temporarily) moved it to now.cpp. Used the perform_benchmark(_impl) function in now.cpp to accurately calculate the timing functions overhead (this now produces stable/reproducible results).

--- modules/unit/include/nt2/sdk/unit/perform_benchmark.hpp
+++ modules/unit/include/nt2/sdk/unit/perform_benchmark.hpp
@@ -11,62 +11,15 @@
 
 #include <nt2/sdk/timing/now.hpp>
 
-#include <vector>
 #include <utility>
 
 namespace nt2 { namespace unit
 {
   typedef std::pair<nt2::details::cycles_t, nt2::details::microseconds_t> benchmark_result_t;
 
-  static std::vector<nt2::details::cycles_t      > individual_measurement_cycles       ;
-  static std::vector<nt2::details::time_quantum_t> individual_measurement_time_quantums;
+  class benchmark_t;
 
-  template <class Test> inline
-  benchmark_result_t perform_benchmark( Test & test, nt2::details::seconds_t const total_duration_in_seconds )
-  {
-    individual_measurement_cycles       .clear();
-    individual_measurement_time_quantums.clear();
-
-    using namespace nt2::details;
-
-    time_quantum_t const total_duration( to_timequantums( total_duration_in_seconds * 1000000 ) );
-    time_quantum_t       duration      ( 0                                                      );
-
-    while ( duration < total_duration )
-    {
-        time_quantum_t const time_start  ( now        () );
-        cycles_t       const cycles_start( read_cycles() );
-        test();
-        cycles_t       const cycles_end  ( read_cycles() );
-        time_quantum_t const time_end    ( now        () );
-
-        cycles_t       const burned_cycles( cycles_end - cycles_start - read_cycles_overhead                                         );
-        time_quantum_t const elapsed_time ( time_end   - time_start   - now_overhead - ( 2 * read_cycles_overhead_in_time_quantums ) );
-
-    /* "other" version:
-        cycles_t       const cycles_start( read_cycles() );
-        time_quantum_t const time_start  ( now        () );
-        test();
-        time_quantum_t const time_end    ( now        () );
-        cycles_t       const cycles_end  ( read_cycles() );
-
-        time_quantum_t const elapsed_time ( time_end   - time_start   - now_overhead                                       );
-        cycles_t       const burned_cycles( cycles_end - cycles_start - read_cycles_overhead - ( 2 * now_overhead_cycles ) );
-    */
-        duration += elapsed_time;
-
-        individual_measurement_cycles       .push_back( burned_cycles );
-        individual_measurement_time_quantums.push_back( elapsed_time  );
-        test.reset();
-    }
-
-    return benchmark_result_t
-    (
-                         median( individual_measurement_cycles        ),
-        to_microseconds( median( individual_measurement_time_quantums ) )
-    );
-  }
-
+  NT2_SDK_TIMING_DECL benchmark_result_t perform_benchmark( benchmark_t & test, nt2::details::seconds_t total_duration_in_seconds );
 } }
 
 #endif
